5d81eaf45831f7ab19362e16fe4ee64f61bca9d4
Abhishekjoshi98 <98544171+Abhishekjoshi98@users.noreply.github.com>
2022-12-03 17:59:10 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <div className="card footer">
       <h2>GitBook | Developed by Abhishek Joshi React Batch Geekster | &copy; 2022</h2>
-      <a href="https://github.com/Abhishekjoshi98/" target="_blank" rel="noreferrer">Github Source Code</a>
+      <a href="https://github.com/Abhishekjoshi98/gitbook/" target="_blank" rel="noreferrer">Github Source Code</a>
       <p> Made with ❤️ &amp; 💻</p>
     </div>
   )
